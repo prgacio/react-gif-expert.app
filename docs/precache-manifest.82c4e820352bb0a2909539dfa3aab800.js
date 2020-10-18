e98c62bc229c1713943b98badc4d4901bf6a1cbb
Patricia Rodríguez <prgacio@gmail.com>
2020-10-19 00:08:12 +0200

    docs agregado

---
+++ docs/precache-manifest.82c4e820352bb0a2909539dfa3aab800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0bd784b411cf329cf6f51fdaceced0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aadb805a064ab1398fc",
+    "url": "/static/css/main.ff6f9cd8.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aadb805a064ab1398fc",
+    "url": "/static/js/main.d6b1d646.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
